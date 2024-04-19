b4cb1ed43704c59c93390932c05c1f9b602aabb7
KinataDewa <dewadafug766@gmail.com>
2024-04-19 12:38:46 +0700

    edit

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -10,6 +10,16 @@
             </div>
         </div>
     </div>
+    
+    {{-- !-- Sidebar user (optional) --> --}}
+  <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+    <div class="image">
+      <img src="{{ asset('adminlte/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+    </div>
+    <div class="info">
+      <a href="#" class="d-block"> Kinata Dewa Ariandi</a>
+    </div>
+  </div>
     <!-- Sidebar Menu -->
     <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
