5e33c148b0979025f311bc9d88a7557bb9a30e63
KinataDewa <dewadafug766@gmail.com>
2024-04-23 11:08:37 +0700

    JS 10 Praktikum1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,28 +6,44 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserModel extends \Illuminate\Foundation\Auth\User
+// class UserModel extends \Illuminate\Foundation\Auth\User
+class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
+    public function getJWTIdentifier()
+    {
+        return $this->getkey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
+    // use HasFactory;
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    // protected $table = 'm_user';
+    // protected $primaryKey = 'user_id';
 
-    public function level(): BelongsTo
-    {
-        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-    }
+    // protected $fillable = ['level_id', 'username', 'nama', 'password'];
 
-    public function stok(): HasMany
-    {
-        return $this->hasMany(StokModel::class, 'user_id', 'user_id');
-    }
+    // public function level(): BelongsTo
+    // {
+    //     return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    // }
 
-    public function transaksi(): HasMany
-    {
-        return $this->hasMany(TransaksiModel::class, 'user_id', 'user_id');
-    }
+    // public function stok(): HasMany
+    // {
+    //     return $this->hasMany(StokModel::class, 'user_id', 'user_id');
+    // }
+
+    // public function transaksi(): HasMany
+    // {
+    //     return $this->hasMany(TransaksiModel::class, 'user_id', 'user_id');
+    // }
 }
